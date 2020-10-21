f775af3602c80229707a85de3491850274c10f01
Josh Burt <dev.josh.burt@icloud.com>
2020-10-20 19:58:26 -0600

    Initial Commit

---
+++ WoofWoof/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoofWoof
+//
+//  Created by Josh Burt on 10/20/20.
+//  Copyright © 2020 Josh Burt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
